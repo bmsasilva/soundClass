b17aaabfb91c5b1864645e891b641b35565a29df
bruno <bmsasilva@gmail.com>
2020-12-02 13:05:29 +0000

    posso ter de reverter esta commit

--- R_old/internal_r.R
+++
@@ -1,9 +0,0 @@
-#' Rotate a matrix
-#' @title Rotate a matrix
-#' @description  Rotate a matrix
-#' @param mat a matrix
-#' @usage r(mat)
-#' @return Rotated matrix
-#' @author Bruno Silva
-#' @keywords internal
-r <- function(mat) apply(mat, 1, rev)
--- extra_files/load_all.R
+++
@@ -1,9 +0,0 @@
-##### Load all #####
-files <- list.files("~/Projectos/R_packages/development/recLabel/R/", pattern = ".R", full.names = TRUE)
-for(file in files) source(file)
-
-files <- list.files("~/Projectos/R_packages/development/Rutils/R/", pattern = ".R", full.names = TRUE)
-for(file in files) source(file)
-
-files <- list.files("~/Projectos/bat_detector/batdecoder_gui/R/", pattern = ".R", full.names = TRUE)
-for(file in files) source(file)
