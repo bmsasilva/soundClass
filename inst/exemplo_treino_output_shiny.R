5c05fa230c1e757fc2361f8cbd3313162fe64be5
bruno <bmsasilva@gmail.comm>
2021-04-29 09:16:58 +0100

    teste

---
+++ inst/exemplo_treino_output_shiny.R
@@ -0,0 +1,37 @@
+library(shiny)
+library(keras)
+
+ui <- fluidPage(
+  shinyjs::useShinyjs(),  # Include shinyjs
+  actionButton("button", "Click me"),
+  textOutput('metrics')
+)
+
+server <- function(input, output) {
+  
+  observeEvent(input$button, {
+    
+    model <- keras_model_sequential() %>%
+      layer_dense(1)
+    
+    model %>%
+      compile(loss = "mse" , optimizer = "adam", metrics = "mse")
+    
+    cb <- callback_lambda(on_epoch_end = function(epoch, logs) {
+      shinyjs::html("metrics", paste("mse: ", str(logs), " epoch:", epoch))
+    })
+    
+    model %>% 
+      fit(
+        x = matrix(runif(100), ncol = 1), 
+        y = matrix(runif(100), ncol = 1),
+        callbacks = list(cb),
+        verbose = 1
+      )
+    
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
