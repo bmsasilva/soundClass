fd84cde57f47e6425775ba9302e722387f067099
bruno <bmsasilva@gmail.comm>
2021-05-04 22:07:14 +0100

    te

--- inst/shiny-app-fit-model/ui.R
+++ inst/shiny-app-fit-model/ui.R
@@ -43,34 +43,25 @@ shinyUI(
                                
                                br(), # introduzir espacamento
                                br(), # introduzir espacamento
-<<<<<<< HEAD
-                               
-                               
-=======
                               
 
->>>>>>> ceab93e9aecfcdae93f14081166d13ca52f2200f
                                
                                # Button 1
                                # ver solucao aqui: https://stackoverflow.com/questions/42945833/getting-file-path-from-shiny-ui-not-just-directory-using-browse-button-without
                                shinyFilesButton('selected_db', 
                                                 'Choose database',
                                                 'Choose database file', FALSE, style='width:100%'),
-                               
+
                                br(), # introduzir espacamento
                                br(), # introduzir espacamento
                                shinyFilesButton('selected_model', 
                                                 'Choose model',
                                                 'Choose model file', FALSE, style='width:100%')
                                
-<<<<<<< HEAD
-                               
-                               
-=======
                               
->>>>>>> ceab93e9aecfcdae93f14081166d13ca52f2200f
                                
                                
+                         
                                
                   ),
                   
@@ -78,59 +69,6 @@ shinyUI(
                   
                   mainPanel(
                     tabsetPanel(id = "inTabset",
-<<<<<<< HEAD
-                                tabPanel("Create train data", 
-                                         value = "panel_plot",
-                                         br(),
-                                         
-                                         fluidRow(
-                                           column(6, align="center", offset = 3,
-                                                  # Button "create db" and respective modal box
-                                                  actionButton("create_specs", "Create spectrograms from labels", style='width:100%'),
-                                                  tags$style(type='text/css', "#create_specs { vertical-align- middle; height- 50px; width- 100%; font-size- 30px;}"),
-                                                  # Code to generate modal boxwith inputs
-                                                  shinyBS::bsModal(id = "modal", Title = "Training spectrograms settings", trigger = "create_specs", size = "small",
-                                                                   HTML("What is the database name?"),
-                                                                   textInput("name", "", ""),
-                                                                   numericInput("spec_size", "", 20, min = NA, max = NA, step = NA, width = NULL ),
-                                                                   ## inserir os parametros para criar os espectrogramas a partir da base de dados
-                                                                   actionButton("conf", "Confirm"))
-                                                  # spec_size <- 20 # ms
-                                                  # window_length <- 1 # em milisegundos
-                                                  # frequency_resolution <- 1 # valor normal e 2
-                                                  # time_step_size <- 0.25
-                                                  # dynamic_range <- 90
-                                                  # freq_range <- c(10, 125) #hertz
-                                                  
-                                                  
-                                           )
-                                         ),
-                                         
-                                         
-                                         plotOutput("spec",
-                                                    height = "auto", #controlar a altura do plot. no sevrer define o tamanho
-                                                    click="specClick",
-                                                    dblclick = "plot1_dblclick", #esta linha e as finais controlam o zoom
-                                                    brush = brushOpts(
-                                                      id = "plot1_brush",
-                                                      resetOnNew = TRUE
-                                                    )),
-                                         
-                                         
-                                         
-                                         fluidRow(column(4,
-                                                         textOutput("db_path")
-                                         )
-                                         ),
-                                         fluidRow(column(4,
-                                                         textOutput("folder_path")
-                                         )
-                                         ),
-                                         fluidRow(column(4,
-                                                         textOutput("model_path")
-                                         )
-                                         )
-=======
                       tabPanel("Create train data", 
                                value = "panel_plot",
                                br(),
@@ -177,68 +115,72 @@ shinyUI(
                                fluidRow(column(4,
                                                textOutput("folder_path")
                                )
->>>>>>> ceab93e9aecfcdae93f14081166d13ca52f2200f
                                 ),
-                                
-                                
-                                tabPanel(title = "Fit model", 
-                                         value = "panel_options", 
-                                         fluidRow(
-                                           column(4,
-                                                  
-                                                  selectInput("dynamicRange",
-                                                              "Threshold",
-                                                              choices = c('40 dB' = '40',
-                                                                          '50 dB' = '50',
-                                                                          '60 dB' = '60',
-                                                                          '70 dB' = '70',
-                                                                          '80 dB' = '80',
-                                                                          '90 dB' = '90',
-                                                                          '100 dB' = '100'),
-                                                              '70')
-                                           ),
-                                           
-                                           column(4,
-                                                  
-                                                  selectInput("windowLength",
-                                                              "Window length",
-                                                              choices = c('1 ms' = '1',
-                                                                          '2 ms' = '2',
-                                                                          '3 ms' = '3',
-                                                                          '4 ms' = '4',
-                                                                          '5 ms' = '5'),
-                                                              '5')
-                                           )
-                                         ),
-                                         fluidRow(
-                                           column(4,
-                                                  
-                                                  selectInput("timeStep",
-                                                              "Overlap",
-                                                              choices = c('60%' = '0.4',
-                                                                          '70%' = '0.3',
-                                                                          '80%' = '0.2',
-                                                                          '90%' = '0.1'),
-                                                              '0.2')
-                                                  
-                                                  
-                                           ),
-                                           
-                                           
-                                           
-                                           column(4,
-                                                  
-                                                  selectInput("freqResolution", "Resolution",
-                                                              choices = c('low resolution' = '1',
-                                                                          'medium resolution' = '4',
-                                                                          'high resolution' = '8'),
-                                                              '4')
-                                           )
-                                           
-                                         ),
-                                         actionButton("save", "Save and update spectrogram"))
-                                
-                                
+                               fluidRow(column(4,
+                                               textOutput("model_path")
+                               )
+                               )
+                      ),
+                      
+                      
+                      tabPanel(title = "Fit model", 
+                               value = "panel_options", 
+                               fluidRow(
+                                 column(4,
+
+                                        selectInput("dynamicRange",
+                                                    "Threshold",
+                                                    choices = c('40 dB' = '40',
+                                                                '50 dB' = '50',
+                                                                '60 dB' = '60',
+                                                                '70 dB' = '70',
+                                                                '80 dB' = '80',
+                                                                '90 dB' = '90',
+                                                                '100 dB' = '100'),
+                                                    '70')
+                                 ),
+
+                                 column(4,
+
+                                        selectInput("windowLength",
+                                                    "Window length",
+                                                    choices = c('1 ms' = '1',
+                                                                '2 ms' = '2',
+                                                                '3 ms' = '3',
+                                                                '4 ms' = '4',
+                                                                '5 ms' = '5'),
+                                                    '5')
+                                 )
+                               ),
+                               fluidRow(
+                                 column(4,
+
+                                        selectInput("timeStep",
+                                                    "Overlap",
+                                                    choices = c('60%' = '0.4',
+                                                                '70%' = '0.3',
+                                                                '80%' = '0.2',
+                                                                '90%' = '0.1'),
+                                                    '0.2')
+
+
+                                 ),
+                                 
+
+                                 
+                                 column(4,
+
+                                        selectInput("freqResolution", "Resolution",
+                                                    choices = c('low resolution' = '1',
+                                                                'medium resolution' = '4',
+                                                                'high resolution' = '8'),
+                                                    '4')
+                                 )
+
+                               ),
+                               actionButton("save", "Save and update spectrogram"))
+                      
+                      
                     )# final tabSetPanel
                     
                   )#Final main panel
@@ -247,9 +189,5 @@ shinyUI(
     
     
   )# final fluid pane
-<<<<<<< HEAD
-)# final shiny ui
-=======
 )# final shiny ui
 
->>>>>>> ceab93e9aecfcdae93f14081166d13ca52f2200f
