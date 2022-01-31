b10571bcadbfe7a00a7e43213cfdd15bf6852302
bmsasilva <bmsasilva@gmail.com>
2022-01-31 15:38:42 +0000

    update docs

--- R/train_metadata.R
+++ R/train_metadata.R
@@ -2,7 +2,7 @@
 #' @description Obtain train metadata from
 #' the output of function \link{spectro_calls}. Needed to run a fitted model 
 #' @param train_data Output of function \link{spectro_calls}.
-#' @usage train_metadata(train_data, save2disk = NA)
+#' @usage train_metadata(train_data)
 #' @return A list with the following components:
 #' \itemize{
 #'   \item parameters -- parameters of the spectrograms
@@ -10,7 +10,7 @@
 #' }
 #' @author Bruno Silva
 #' @export
-train_metadata <- function(train_data, save2disk = NA){
+train_metadata <- function(train_data){
   metadata <- list(parameters =  train_data$parameters,
                    classes = train_data$classes)
   return(metadata)
